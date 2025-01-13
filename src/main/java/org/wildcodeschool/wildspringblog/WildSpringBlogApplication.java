b1533813f1166a01f5a01335bf8a4b8849d3bb86
jadejolivet <jade.jolivet@littleworker.fr>
2025-01-13 12:43:55 +0100

    Add dependency

---
+++ src/main/java/org/wildcodeschool/wildspringblog/WildSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package org.wildcodeschool.wildspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildSpringBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wildcodeschool/wildspringblog/WildSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildcodeschool.wildspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
